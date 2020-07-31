047f80e7db7b1043b350cd4d1001c206e3e3c76e
hz <15738818183@163.com>
2020-07-31 18:13:04 +0800

    feat: init project

---
+++ src/components/element.js
@@ -0,0 +1,72 @@
+import {
+  Input,
+  InputNumber,
+  Radio,
+  RadioGroup,
+  RadioButton,
+  Checkbox,
+  CheckboxButton,
+  CheckboxGroup,
+  Switch,
+  Select,
+  Option,
+  OptionGroup,
+  Button,
+  ButtonGroup,
+  Message,
+  Col,
+  Row,
+  Table,
+  TableColumn,
+  Dialog,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Breadcrumb,
+  BreadcrumbItem,
+  Backtop,
+} from 'element-ui'
+
+const components = [
+  Input,
+  InputNumber,
+  Radio,
+  RadioGroup,
+  RadioButton,
+  Checkbox,
+  CheckboxButton,
+  CheckboxGroup,
+  Switch,
+  Select,
+  Option,
+  OptionGroup,
+  Button,
+  ButtonGroup,
+  Message,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  InputNumber,
+  Dialog,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Breadcrumb,
+  BreadcrumbItem,
+  Backtop,
+]
+
+const install = function install (Vue) {
+  if (install.installed) return
+
+  components.forEach((component) => {
+    Vue.component(component.name, component)
+  })
+}
+
+export default {
+  install
+}
